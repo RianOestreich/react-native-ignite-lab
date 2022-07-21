19df3d0b089d8b539b32ea53bf3982cd6f3c7124
Rian Gustavo Oestreich <riangoestreich@gmail.com>
2022-07-20 21:53:54 -0300

    Aula1

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,12 +1,17 @@
 import { VStack, Heading, Icon, useTheme } from 'native-base';
 import { Envelope, Key} from 'phosphor-react-native'
+import { useState } from 'react';
 
 import Logo from '../assets/logo_primary.svg'
 import { Input } from '../components/Input';
 import { Button } from '../components/Button';
 
 export function SignIn(){
+    const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
+
     const { colors } = useTheme();
+
     return(
         <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>  
             <Logo />
@@ -19,12 +24,14 @@ export function SignIn(){
                 placeholder="E-mail"
                 mb={4}
                 InputLeftElement={<Icon as={<Envelope color={colors.blueGray[300]} />} ml={4}/>}
+                onChangeText={setName}
             />
             <Input 
                 placeholder="Senha"
                 mb={8}
                 InputLeftElement={<Icon as={<Key color={colors.blueGray[300]} />} ml={4}/>}
                 secureTextEntry
+                onChangeText={setPassword}
             />
 
             <Button title="Entrar" w="full"/>
